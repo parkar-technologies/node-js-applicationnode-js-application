a3ef300eedb3866cbd7cf301dd3570ea2976ba74
wizdomtechnolgys <wizdomtechnologys@gmail.com>
2023-12-10 20:03:43 +0530

    node-js-application

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.0.76.172:9000/',
+  serverUrl: 'http://192.168.197.138:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '9bcdd15299583c728cf53837f2ab21fcb36c5aa4',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    'sonar.projectKey':'NodeJsWizdomTechnologies',
+	    //'sonar.login': '9bcdd15299583c728cf53837f2ab21fcb36c5aa4',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
